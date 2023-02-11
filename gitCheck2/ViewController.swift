c136474e313b2ca57b67d08ff5277a0108b398b0
황홍필 <marigold410@naver.com>
2023-02-11 16:24:10 +0900

    gitCheck2
    함수 하나 추가했음

--- gitCheck2/ViewController.swift
+++ gitCheck2/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    func doSomething() {
+        print("뭔가를 할 거야~~~")
+    }
 
 
 }
